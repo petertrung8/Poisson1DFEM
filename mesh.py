fc96656171fafd3e7e53daea0f003c085765fbce
Peter Mai <mttrung94@gmail.com>
2024-01-10 22:14:03 +0900

    Created the mesh function and random mesh function

---
+++ mesh.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def createMesh(n, omega=(0, 1)):
+    """Return an array of n nodes that are 
+    equally spaced in the domain omega"""
+    return np.linspace(omega[0], omega[1], n)
+
+
+def createRandMesh(n, omega=(0, 1)):
+    """Return an array of n nodes that are 
+    randomly spaced in the domain omega"""
+    return np.sort(np.random.uniform(low=omega[0], high=omega[1], size=(n,)))
