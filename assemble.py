d1e2db1e1037d2a9dfb1fd6e4bb27b0c0c215a3b
Peter <mttrung94@gmail.com>
2024-01-11 09:33:41 +0900

    Created the load and stiffness assembly functions

---
+++ assemble.py
@@ -0,0 +1,37 @@
+import numpy as np
+import scipy.sparse as sp
+from basis import *
+
+def loadAssembly(x, f):
+    """Assemble and return the load vector
+    for the 1D mesh x and force f""" 
+    N = x.shape[0]
+    F = np.zeros((N, 1))
+    F[0] = intHat1(x[0], x[1], f)
+    F[-1] = intHat1(x[-2], x[-1], f)
+    
+    for i in range( N-1):
+        F[i] = F[i] + intHat2(x[i], x[i+1], f)
+        F[i+1] = F[i+1] + intHat1(x[i], x[i+1], f)
+    return F
+
+
+def stiffnessAssembly(x):
+    """Assemble and return a sparse stiffness matrix  A
+    for the given 1D mesh x""" 
+    N = x.shape[0]
+    h = x[1:]-x[0:-1] # get the element sizing
+    
+    # initiate a sparse A matrix and the first and last element of the diagonal
+    A = sp.lil_matrix((N, N), dtype=np.float64)
+    A[0, 0] = 1/h[0]
+    A[-1, -1] = 1/h[-1]
+    
+    # loop through the diagonal to 
+    for i in range(N-1):
+        A[i,i] += 1/h[i]
+        A[i,i+1] -= 1/h[i]
+        A[i+1,i] -= 1/h[i]
+        A[i+1,i+1] += 1/h[i]
+    return A
+    
