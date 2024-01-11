306823400af1ba2cfc9569d9ffbd0c117e7a0ad3
Peter <mttrung94@gmail.com>
2024-01-11 21:28:51 +0900

    Added the plotting of the results and error plot

--- poissonFEMsolve.py
+++ poissonFEMsolve.py
@@ -4,16 +4,16 @@ import scipy.sparse as sp
 from mesh import *
 from assemble import *
 from boundary import *
+from utils import *
 
 # specify the domain and the force field f
-omega = (0, 1)
-d_bc = [(0, 0), (1, 0)]
-f = lambda x: x * ( x + 3 ) * np.exp ( x )
+omega = (0, 1) # domain
+d_bc = [(0, 1), (1, 1)] # list of tuples of Dirichlet BC, each tuple is (location, value)
+f = lambda x: x * ( x + 3 ) * np.exp ( x ) # the source function
 
 # specifying the number of elements to discretize the domain
-n_e = 100
+n_e = 5
 n_n = n_e + 1
-
 x = createMesh(n_n, omega=omega)
 
 # assemble the stiffness matrix and load vector
@@ -21,13 +21,30 @@ A = stiffnessAssembly(x)
 F = loadAssembly(x, f)
 
 # apply the Dirichlet boundary conditions
-A_bc, F_bc = DirichletHomBC(x, A, F, d_bc)
+A_bc, F_bc = DirichletBC(x, A, F, d_bc)
 
 # solve the linear system of equations
+A_bc = sp.csr_matrix(A_bc) # casting the matrix into csr matrix to make it faster
 U = sp.linalg.spsolve(A_bc, F_bc)
 
-# plot the solution
-x2 = np.arange(0, 1.05, 0.05)
-# plt.plot(x2, x2*(1-x2)/2, '-')
-plt.plot(x2, x2 * ( 1 - x2 ) * np.exp ( x2 ), '-')
-plt.plot(x, U, '--o')
+# plot the solution and compare with exact solution
+x2 = np.linspace(0, 1, 101)
+s = x2.shape[0]
+u_exact = np.zeros((s, 1))
+u_fem = np.zeros((s, 1))
+for i in range(s):
+    u_exact[i] = exact(x2[i])
+    u_fem[i] = fem_soln(x, U, x2[i])
+    
+plt.figure()
+plt.plot(x2, u_exact, '-')
+plt.plot(x2, u_fem, '--')
+plt.xlabel('x')
+plt.ylabel('u(x)')
+plt.title('Solid line: Exact solution, Dashed line: FE solution')
+
+plt.figure()
+plt.title('Error plot')
+plt.plot(x2, u_fem - u_exact)
+plt.xlabel('x')
+plt.ylabel('$U(x) -U_{fem}(x)$')
