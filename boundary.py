7c6dd9c1b411a624491324c79ce35af1a97057db
Peter <mttrung94@gmail.com>
2024-01-17 21:28:35 +0900

    Changed the Diriclet BC to take domain and boundary values separately

--- boundary.py
+++ boundary.py
@@ -2,16 +2,16 @@ import numpy as np
 import copy
 
 
-def DirichletBC(x, A, F, bc=[(0,0), (1, 0)]):
+def DirichletBC(x, A, F, loc, bc):
     """Apply the Dirichlet BC to the stiffness and load matrix and 
     return the new A, F"""
     n_bc = len(bc)
     
     # find the row and column that correspond to the boundary condition
-    idx = [int(np.where(x==i[0])[0]) for i in bc]
+    idx = [int(np.where(x==i)[0]) for i in loc]
     for i in range(n_bc):
-        F = F - A[:, idx[i]]*bc[i][1]
-        F[idx[i]] = bc[i][1]
+        F = F - A[:, idx[i]]*bc[i]
+        F[idx[i]] = bc[i]
     
     for i in range(n_bc):
         A[:, idx[i]] = 0
